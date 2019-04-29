7fcfff432385f46c3382611196eb4b0e8b09dce9
Soeren Schmidt <mailboxsoeren@gmail.com>
2019-04-29 08:46:45 +0200

    initial

---
+++ app.js
@@ -0,0 +1,8 @@
+const inputTitle = document.querySelector('#title');
+const inputQuantity = document.querySelector('#quantity');
+const btnSave = document.querySelector('#btn-save');
+const btnDelete = document.querySelector('#btn-delete');
+
+btnSave.addEventListener('click', () => {
+    console.log('saved');
+})
